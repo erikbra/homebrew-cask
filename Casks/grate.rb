c39489d593922da9325c29b93ac6195043fa198c
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2023-04-06 13:06:31 +0200

    Added both architectures

--- Casks/grate.rb
+++ Casks/grate.rb
@@ -1,11 +1,18 @@
 cask "grate" do
-    version "1.4.0"
-    sha256 "091b03b0d2a7c6b0146eeca6f05ffdc2cf337d5c67cf79557b2ce47554f6975c"
+    version "1.5.0"
 
-    url "https://github.com/erikbra/grate/releases/download/#{version}/grate-osx-x64-self-contained-#{version}.zip"
     name "grate"
     desc "grate - the SQL scripts migration runner"
     homepage "https://erikbra.github.io/grate/"
+
+    if Hardware::CPU.arm?
+      url "https://github.com/erikbra/grate/releases/download/#{version}/grate-osx.13-arm64-self-contained-#{version}.zip"
+      sha256 "726bebb02dae910bb7068b8caff5921c090185c5a59a34ab1aad07a0b9e7cf34"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/erikbra/grate/releases/download/#{version}/grate-osx-x64-self-contained-#{version}.zip"
+      sha256 "e8614336b692f741e0f0913374dce98c2a7a0ff55e71fb9eee588607adbe1c12"
+    end
 
     binary "grate"
 
