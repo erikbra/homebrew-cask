40c58415d7382e1670f1b62ae56d609fedb4f71e
Erik A. Brandstadmoen <erik.brandstadmoen@komplettbank.no>
2024-03-13 19:14:38 +0100

    grate -> 1.6.2

--- Casks/grate.rb
+++ Casks/grate.rb
@@ -1,5 +1,5 @@
 cask "grate" do
-    version "1.6.1"
+    version "1.6.2"
 
     name "grate"
     desc "grate - the SQL scripts migration runner"
@@ -7,11 +7,11 @@ cask "grate" do
 
     if Hardware::CPU.arm?
       url "https://github.com/erikbra/grate/releases/download/#{version}/grate-osx-arm64-self-contained-#{version}.zip"
-      sha256 "5b4c0b754924642e5edd4a87d48e64303be3af3dec824c371ec5b567aca3cdae"
+      sha256 "358c36a1b76e27d7eab291c34725600b23ab46d7add9b1c28a8ad30a54d4d00b"
     end
     if Hardware::CPU.intel?
       url "https://github.com/erikbra/grate/releases/download/#{version}/grate-osx-x64-self-contained-#{version}.zip"
-      sha256 "a6d1489e03262b97e7d10fb95d8fa426936dd6006580925feb7ac76987a83ff9"
+      sha256 "b16a878e2d65d2fb5a7d2aa43b17f4e5cc2a9e19e76f08bf512a764523c666f9"
     end
 
     binary "grate"
